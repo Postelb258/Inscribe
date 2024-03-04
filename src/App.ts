e859ff21120774a1fb4947323810a9e44ff83d8d
Postelb258 <clewith@gmail.com>
2024-03-04 19:31:57 +0400

    Updated:textbook:

--- src/App.ts
+++ src/App.ts
@@ -1,3 +1,4 @@
+import { SQLiteRepositoryAdapter } from '@adapters/SQLiteRepositoryAdapter'
 import { Bot } from '@core/Bot'
 import { HelpBuilder } from '@core/HelpBuilder'
 import { StartCommand } from '@core/commands/start'
@@ -5,9 +6,18 @@ import { pino, type Logger } from 'pino'
 
 const logger: Logger = pino()
 
+const repositoryAdapter = new SQLiteRepositoryAdapter(
+    logger,
+    'journalRepository.sqlite'
+)
+
 const bot = new Bot(logger, process.env.BOT_TOKEN)
     .registerStartCommand(
-        new StartCommand('start', 'Start interacting with bot')
+        new StartCommand(
+            repositoryAdapter,
+            'start',
+            'Start interacting with bot'
+        )
     )
     .registerHelp(new HelpBuilder())
-    .run()
+    .run(() => repositoryAdapter.close())
