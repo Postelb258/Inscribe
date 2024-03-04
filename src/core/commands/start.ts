b9cc48f53abaf43e010122438fa5c62cc7dd8345
Postelb258 <clewith@gmail.com>
2024-03-04 19:33:16 +0400

    + createChat(..)

--- src/core/commands/start.ts
+++ src/core/commands/start.ts
@@ -1,8 +1,21 @@
 import { Command } from '@core/Command'
+import type { RepositoryPort } from '@ports/RepositoryPort'
 import type { Context } from 'telegraf'
 
 export class StartCommand<T extends Context> extends Command<T> {
+    repositoryAdapter: RepositoryPort
+    constructor(
+        repositoryAdapter: RepositoryPort,
+        name: string,
+        help: string,
+        args?: string
+    ) {
+        super(name, help, args)
+
+        this.repositoryAdapter = repositoryAdapter
+    }
     handle(ctx: T): void {
+        if (ctx.chat) this.repositoryAdapter.createChat(ctx.chat.id)
         ctx.reply(`🔐 Hello, Welcome to Inscribe, where you can keep your journal records confidentially! 📔✨
 
 With this bot, you can securely keep track of various records and create personalized patterns for different types of entries. 📈✒️ Whether it's daily thoughts, goals, or important events 💼
